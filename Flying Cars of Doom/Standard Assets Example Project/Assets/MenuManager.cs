c791efe31c93cbffe30eb8f05d02ef4435547c0d
7052roy <leinenga.r@hotmail.nl>
2018-01-14 17:22:38 +0100

    menu and deer

---
+++ Flying Cars of Doom/Standard Assets Example Project/Assets/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public string scene;
+    public Color loadToColor = Color.black;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        Initiate.Fade(scene, loadToColor, 0.7f);
+    }
+
+    
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
